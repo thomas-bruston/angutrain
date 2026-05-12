9d7339c8fde6e16fc42fdb781cb537a555ff9a35
vitegourmand2026 <tombruston@gmail.com>
2026-05-12 12:17:03 +0200

    init - setup projet Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Produit} from './produit';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('angutrain');
+
+constructor() {
+  const laptop = new Produit(1,"AppleMacBook",1200,true,"Portable");
+  const phone = new Produit(2,"Samsung",700,false,"Mobile");
+  const tablet = new Produit(3,"Ipad",600,true);
+  console.log (laptop.IsEnStock());
+  console.log (phone.IsEnStock());
+  console.log(tablet.appliquerRemise(20));
+  console.log(phone.appliquerRemise(10));
+  console.log(laptop.appliquerRemise(50));
+
+}
 }
---
+++ src/app/produit.ts
@@ -0,0 +1,45 @@
+export interface IProduit {
+    id:number;
+    nom:string;
+    prix:number;
+    stock: boolean;
+    categorie?:string;
+}
+
+const Laptop : IProduit =  {
+    id: 1,
+    nom: "Apple",
+    prix: 1200,
+    stock : true,
+    categorie : "Portable",
+
+
+}
+
+export class Produit implements IProduit {
+    id: number;
+    nom : string;
+    prix : number;
+    stock : boolean;
+    categorie?: string;
+
+
+
+constructor (id:number,nom:string,prix:number,stock:boolean,categorie?:string){
+    this.id = id;
+    this.nom = nom;
+    this.prix = prix;
+    this.stock = stock;
+    this.categorie = categorie;
+}
+
+IsEnStock():boolean{
+    return this.stock 
+
+}
+appliquerRemise(pourcentage:number):number{
+    return this.prix *(1-pourcentage/100)
+
+
+}}
+
