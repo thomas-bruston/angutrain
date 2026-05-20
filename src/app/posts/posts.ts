38dc9b9f879e631bdcaf597dcf9f34265ae2e6be
vitegourmand2026 <tombruston@gmail.com>
2026-05-20 16:13:54 +0200

    J13 - RxJS bases, opérateurs pipe map filter tap

--- src/app/posts/posts.ts
+++ src/app/posts/posts.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../post-service';
+import { FormsModule } from '@angular/forms';
+import { map } from 'rxjs/operators';
 
 interface IPost{
   id : number,
@@ -11,21 +13,29 @@ interface IPost{
 @Component({
   selector: 'app-posts',
   standalone : true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './posts.html',
   styleUrl: './posts.css',
 })
 export class Posts implements OnInit{
   posts : IPost[] = [];
+  postsFiltres : IPost[] = [];
+  recherche : string = '';
 
   constructor(private postService : PostService) {}
   ngOnInit(): void {
-    this.postService.getPosts().subscribe(posts =>{
+    this.postService.getPosts()
+    .pipe(map(posts =>posts.slice(0,20)))
+    .subscribe(posts =>{
       this.posts= posts;
-      console.log('Post recus : ' , this.posts);
+      this.postsFiltres = posts;
+      
     });
-
-    
+  }
+  onRecherche() : void{
+    this.postsFiltres = this.posts.filter(p =>
+      p.title.toLowerCase().includes(this.recherche.toLowerCase())
+    );
   }
 
 }
