552efb081f405b73dbe0e3bc4eccfced509ffe6b
vitegourmand2026 <tombruston@gmail.com>
2026-05-21 23:08:09 +0200

    J16 - Routing, navigation entre pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Accueil } from './accueil/accueil';
+import { Blog } from './blog/blog';
+import { ListeProduits } from './liste-produits/liste-produits';
+import { Notifications } from './notifications/notifications';
+import { Posts } from './posts/posts';
+import { Profil } from './profil/profil';
+import { Todo } from './todo/todo';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {path: '' , component:Accueil},
+    {path: 'blog' , component:Blog},
+    {path: 'liste-produits' , component:ListeProduits},
+    {path:'notifications' , component:Notifications},
+    {path: 'posts' , component:Posts},
+    {path: 'Profil' , component:Profil},
+    {path:'todo' , component:Todo},
+    
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { IPost,IUser,getPosts,getPostsByUser,getUserById } from './api-client';
-import { Compteur } from "./compteur/compteur";
-import { Todo } from './todo/todo';
-import { ListeProduits } from "./liste-produits/liste-produits";
-import { Profil } from './profil/profil';
-import { Posts } from './posts/posts';
-import { Notifications } from './notifications/notifications';
-import { Blog } from "./blog/blog";
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Compteur, Todo, ListeProduits, Profil, Posts, Notifications, Blog],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  titre ='Mon porte folio Angular'
-  description = 'devellopeur Angular en formation'
-  annee = new Date().getFullYear();
+  
 } 
