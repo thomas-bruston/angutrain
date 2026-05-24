e6b2e172258fbde902a4d7f2372a814f311e2c9b
vitegourmand2026 <tombruston@gmail.com>
2026-05-24 19:20:39 +0200

    J25 - Portfolio, README, page d'accueil

--- src/app/accueil/accueil.ts
+++ src/app/accueil/accueil.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-accueil',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './accueil.html',
   styleUrl: './accueil.css',
 })
